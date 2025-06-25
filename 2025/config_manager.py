5fd3f7841e6096ce458aea9d4f368e7b06cb28f4
Alexey Rumyantsev <alexeyrumyantsev04@gmail.com>
2025-06-25 09:49:04 +0300

    added сfg impl

---
+++ 2025/config_manager.py
@@ -0,0 +1,28 @@
+import yaml
+
+
+class Config:
+    ip: str = ""
+    port: int = 0
+
+    @classmethod
+    def load(cls, path: str = "config.yaml"):
+        with open(path, "r") as file:
+            config = yaml.safe_load(file)
+            cls.ip = config["connection"]["ip"]
+            cls.port = config["connection"]["port"]
+
+    @classmethod
+    def save(cls, path: str = "config.yaml"):
+        with open(path, "w") as file:
+            yaml.safe_dump({
+                "connection": {
+                    "ip": cls.ip,
+                    "port": cls.port
+                }
+            }, file)
+
+    @classmethod
+    def set_config(cls, ip: str, port: int):
+        cls.ip = ip
+        cls.port = port
--- 2025/main.py
+++ 2025/main.py
@@ -1,10 +1,13 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from config_manager import Config
 
 import main_window as mw
 
 
 if __name__ == "__main__":
+    Config.load()
+
     app = QApplication(sys.argv)
 
     window = mw.MainWindow()
